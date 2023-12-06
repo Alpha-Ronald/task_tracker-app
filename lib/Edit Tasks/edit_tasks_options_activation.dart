f234cbc64067518fb125985563a4ae875a301c0a
Alpha-Ronald <ronnieadewoye@gmail.com>
2023-12-06 15:43:31 +0100

    Conditional Activation for task buttons in settings(all buttons completed)

--- lib/Edit Tasks/edit_tasks_options_activation.dart
+++ lib/Edit Tasks/edit_tasks_options_activation.dart
@@ -19,12 +19,21 @@ class EditTaskOptions extends StatefulWidget {
 class _EditTaskOptionsState extends State<EditTaskOptions> {
   @override
   Widget build(BuildContext context) {
+    // habits button conditional activation
     Color habitsButtonTextColor = widget.showHabitsList
         ? const Color(0xFF1D364D)
         : const Color(0xFF1D364D).withOpacity(0.2);
+    Color habitsButtonBorderColor = widget.showHabitsList
+        ? Colors.black
+        : const Color(0xFF1D364D).withOpacity(0.1);
+
+    // to-do button conditional activation
     Color todoButtonTextColor = !widget.showHabitsList
         ? const Color(0xFF1D364D)
         : const Color(0xFF1D364D).withOpacity(0.2);
+    Color todoButtonBorderColor = !widget.showHabitsList
+        ? Colors.black
+        : const Color(0xFF1D364D).withOpacity(0.1);
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -33,16 +42,21 @@ class _EditTaskOptionsState extends State<EditTaskOptions> {
           height: 70,
           width: 175,
           child: ElevatedButton(
-              //logic disabling button when it is active
+            //logic disabling button when it is active
               onPressed: widget.showHabitsList ? null : widget.onHabitPressed,
               style: ElevatedButton.styleFrom(
+                //
+                  side: BorderSide(color: habitsButtonBorderColor, width: 1),
+                  //
+
                   elevation: 0,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(8)),
                   backgroundColor: const Color(0xFF1D364D).withOpacity(0.1)),
               child: Text(
                 'Habits',
-                style: Theme.of(context)
+                style: Theme
+                    .of(context)
                     .textTheme
                     .displaySmall
                     ?.copyWith(fontSize: 20, color: habitsButtonTextColor),
@@ -54,13 +68,15 @@ class _EditTaskOptionsState extends State<EditTaskOptions> {
           child: ElevatedButton(
               onPressed: !widget.showHabitsList ? null : widget.onTodoPressed,
               style: ElevatedButton.styleFrom(
+                  side: BorderSide(color: todoButtonBorderColor, width: 1),
                   elevation: 0,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(8)),
                   backgroundColor: const Color(0xFF1D364D).withOpacity(0.1)),
               child: Text(
                 'To Do',
-                style: Theme.of(context)
+                style: Theme
+                    .of(context)
                     .textTheme
                     .displaySmall
                     ?.copyWith(fontSize: 20, color: todoButtonTextColor),
