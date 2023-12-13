bd64fecc78353ce0798faafd13e796df092aefe3
Alpha-Ronald <ronnieadewoye@gmail.com>
2023-12-13 10:19:17 +0100

    Revert "Starting listBuilder for HabitEdits"
    This reverts commit 4f43ec73f1756426f3388b43e02195e5cba7067d.
    
    # Conflicts:
    #	lib/Create New Habit/create_new_habit_page.dart
    #	lib/Edit Tasks/settigs_tasks_tiles.dart

--- lib/Home Page/buttons.dart
+++ lib/Home Page/buttons.dart
@@ -14,9 +14,7 @@ class AddNewTaskButton extends StatelessWidget {
           Navigator.push(
               context,
               MaterialPageRoute(
-                  builder: (context) => const CreateNewHabitPage(
-                        buttonTitle: 'Create a new habit',
-                      )));
+                  builder: (context) => const CreateNewHabitPage()));
         },
         backgroundColor: const Color(0xFF1D364D),
         tooltip: 'Add new Habit/Task',
